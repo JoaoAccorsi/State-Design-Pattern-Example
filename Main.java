8e9272868e98b14c6a1eada694222e093bcecc6f
JoaoAccorsi <60155867+JoaoAccorsi@users.noreply.github.com>
2023-04-16 11:59:40 -0300

    Adding Main Class

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Despertador despertador = new Despertador(20, 20);
+        despertador.armar();
+        //despertador.setPessoaDesarmou(true);
+        despertador.desarmar();
+
+    }
+}
